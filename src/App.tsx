e04b88e9ae1783e605ea123e52cf77c9b34d70d6
Jun Araki <105146615+Jun-Araki@users.noreply.github.com>
2025-02-11 22:02:20 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./styles.css";
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user: User = {
+  name: "JUN",
+  // hobbies: ["プログラミング", "英語"],
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        res.data.map((todo) => todo.userId);
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/Todo.tsx
@@ -0,0 +1,7 @@
+import { TodoType } from "./types/todo";
+
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed = false } = props;
+  const completedMark = completed ? "[完]" : "[未]";
+  return <p>{`${completedMark}${title}(ユーザー:${userId})`}</p>;
+};
